07ecbad630082be48c5a0501212db4ad215f5e32
Соколов Иван <i.sokolov@smartis.bi>
2025-07-04 13:40:53 +0300

    new_format_2

--- webhook_test.py
+++ webhook_test.py
@@ -1,12 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-@app.route('/tilda-webhook', methods=['POST'])
+@app.route("/tilda-webhook", methods=["POST"])
 def webhook():
-    data = request.json
+    data = request.form.to_dict()
     print("Получен Webhook от Tilda:", data)
-    return {"status": "ok"}
-
-if __name__ == '__main__':
-    app.run(port=8000)
+    with open("log.txt", "a", encoding="utf-8") as f:
+        f.write(json.dumps(data, ensure_ascii=False) + "\n")
+    return jsonify(status="ok")
