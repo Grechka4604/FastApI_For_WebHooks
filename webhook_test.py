bb0b66e9028721aa31190866809ec6ad10a0e08e
Соколов Иван <i.sokolov@smartis.bi>
2025-07-04 15:18:13 +0300

    Добавил матомбу

--- webhook_test.py
+++ webhook_test.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 @app.route("/tilda-webhook", methods=["POST"])
 def webhook():
     data = request.form.to_dict()
+    print.pprint(data)
     print("Получен Webhook от Tilda:", data)
     with open("log.txt", "a", encoding="utf-8") as f:
         f.write(json.dumps(data, ensure_ascii=False) + "\n")
+    return jsonify(status="ok")
+
+@app.route("/matomba-webhook", methods=["POST"])
+def matomba():
+    data = request.get_json(force=True)
+    print("🔥 Пришёл Webhook от Matomba:", data)
     return jsonify(status="ok")
