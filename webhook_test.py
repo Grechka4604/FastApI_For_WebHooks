05ac1091b7d471e6ef2da54e0e00d577d9b73b08
Ivan Sokolov <113102138+Grechka4604@users.noreply.github.com>
2025-07-04 17:17:50 +0300

    Update webhook_test.py

--- webhook_test.py
+++ webhook_test.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route("/tilda-webhook", methods=["POST"])
 def webhook():
     data = request.form.to_dict()
-    print.pprint(data)
     print("Получен Webhook от Tilda:", data)
     with open("log.txt", "a", encoding="utf-8") as f:
         f.write(json.dumps(data, ensure_ascii=False) + "\n")
